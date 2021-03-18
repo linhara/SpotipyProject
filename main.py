641aa5f48793c184c46938b84623f79a08728d1d
NazifTheDon <nazif.kadiroglu1@fmail.com>
2021-03-18 00:24:48 +0000

    cleaned more comments

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from spotipy.oauth2 import SpotifyOAuth
 
 Sp = sp.Spotify(auth_manager=SpotifyOAuth(client_id="5d6ba60172a84bf2966d5a073b3c02d1",
                                                client_secret="c898a8f530f5478bb5420ec418326421",
-                                               redirect_uri="https://spotifycharts.com/regional/global/weekly/latest",#"http://localhost:9000",
+                                               redirect_uri="https://spotifycharts.com/regional/global/weekly/latest",
                                                scope="user-library-read"))
 
-results = Sp.current_user_playlists()
+results = Sp.album_tracks("https://spotifycharts.com/regional/global/weekly/latest")
 print(results)
 for thing in results.get("items"):
     print(f"playlist: {thing.get('name')} with tracks: {thing.get('tracks')}")
