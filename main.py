0a3eff3afffce15114624705f71c1687323cb747
linhara <linus_haraldsson@hotmail.com>
2021-03-19 14:55:54 +0100

    working OAauth

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import spotipy as sp
-
 from spotipy.oauth2 import SpotifyOAuth
-OAuth = SpotifyOAuth(client_id="de445576e2884c55be39229d878d7ca1",
-                                               client_secret="66e060d86a3e4f78bdd4e8a65886d3fe",
-                                               redirect_uri="http://basicbitch",#"http://localhost:9000",
-                                               scope="user-library-read")
-Sp = sp.Spotify(auth_manager=OAuth)
-token = OAuth.get_cached_token()
+from spotipy.oauth2 import SpotifyClientCredentials
 
-playlists = Sp.current_user_playlists()
-print(playlists)
+def main():
+    #username also necessary to see which playlists are actually yours in future
+    uName = input("input Spotify Username: ")
+    OAuth = sp.oauth2.SpotifyPKCE(client_id="de445576e2884c55be39229d878d7ca1",
+                                                   username = uName,
+                                                   redirect_uri="http://localhost:9000",#"http://localhost:9000",
+                                                   scope="user-read-private user-library-read",)
 
-for playlist in playlists.get("items"):
-    albumTrackList = Sp.playlist(playlist.get('id')).get('tracks').get('items')
-    print(f"album: {Sp.playlist(playlist.get('id')).get('name')}")
-    for track in albumTrackList:
-        if track.get('track') != None:
-            print(track.get('track').get('name'))
+    Sp = sp.Spotify(auth_manager=OAuth)
+
+
+    playlists = Sp.current_user_playlists()
+    print(playlists)
+
+    for playlist in playlists.get("items"):
+        albumTrackList = Sp.playlist(playlist.get('id')).get('tracks').get('items')
+
+        for track in albumTrackList:
+            if track.get('track'):
+                print(track.get('track').get('name'))
+
+
+if __name__ == "__main__":
+    main()
 
