c917152aee3314185f8067d746eaea13cd426608
NazifTheDon <nazif.kadiroglu1@fmail.com>
2021-03-19 14:09:41 +0000

    sharing is caring

--- main.py
+++ main.py
@@ -4,31 +4,40 @@ import spotipy.util as util
 
 playlist_url ="https://open.spotify.com/playlist/2YRe7HRKNRvXdJBp9nXFza?code=AQAQZcBQj5p_5ry1PxdeO6oLjBzIkd67MVLfhQul_0HMliIW5k3sx9OEtGWAuswEa94EjUpgZdQEhTYQttclGfHZN6boYLI4zDwu4CERkVpfY0W1FopucXdkhKnV0oPD_-kuv4yKrHI9VkhTSH_XuN5NPiBD8roa9pp_Eclw1I1mE_td9OH_urKanteQtvI8P6d_ZSiueUnRgJ17oZsjqNlLAVHlFxhb1CGlv57fKg"
 
-lmao = "http://basicbitch"
-OAuth = SpotifyOAuth(client_id="5d6ba60172a84bf2966d5a073b3c02d1",
-                                               client_secret="c898a8f530f5478bb5420ec418326421",
-                                               redirect_uri="http://basicbitch",
+userName = input("Enter Username: ")
+OAuth = sp.oauth2.SpotifyPKCE(client_id="5d6ba60172a84bf2966d5a073b3c02d1",
+                                               username= userName,
+                                               redirect_uri="http://localhost:9000",
                                                scope="user-library-read")
+
 Sp = sp.Spotify(auth_manager=OAuth)
 for i in range(6):
     tracks = Sp.playlist_items(playlist_url, offset = 100 * 1).get('items')
 totalPopularity = 0
 
 sumOfSongs = 0
-number= tracks[0].get('track').get('popularity')
-#print(tracks[0].get('track').get('popularity'))
 for item in tracks:
     if item.get('track'):
         sumOfSongs +=1
-        #print(f'{item.get("track").get("name")}: {item.get("track").get("popularity")}')
         totalPopularity += item.get("track").get("popularity")
 
 averagePopularity = totalPopularity/sumOfSongs
 
-if number >= averagePopularity:
-    print('You are a basic bitch')
-else:
-    print('You are not a basic bitch')
+userPlaylists = Sp.current_user_playlists().get('items')
+userProfile   = Sp.user(userName)
+print(userProfile)
+
+popularity = 0
+for playlist in userPlaylists:
+
+    displayName = Sp.playlist(playlist.get('id')).get('owner').get('display_name')
+
+    print(displayName)
+    for item in displayName:
+        #if item.get('track'):
+        nameOfSongs = item.get('track').get('name')
+        popularity += item.get('track').get('popularity')
+        print(nameOfSongs)
 
 
 
