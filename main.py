10a622b16f1aebf737e2257a242bf5e18d3fa027
linhara <linus_haraldsson@hotmail.com>
2021-03-19 18:05:17 +0100

    find all songs on all playlists, not only yours

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def main():
-    #username also necessary to see which playlists are actually yours in future
     uName = input("input Spotify Username: ")
     OAuth = sp.oauth2.SpotifyPKCE(client_id="de445576e2884c55be39229d878d7ca1",
                                                    username = uName,
+                                                   #username = 'linusharaldsson1',
                                                    redirect_uri="http://localhost:9000",#"http://localhost:9000",
                                                    scope="user-read-private user-library-read",)
 
@@ -16,12 +16,24 @@ def main():
     playlists = Sp.current_user_playlists()
     print(playlists)
 
-    for playlist in playlists.get("items"):
-        albumTrackList = Sp.playlist(playlist.get('id')).get('tracks').get('items')
 
-        for track in albumTrackList:
-            if track.get('track'):
-                print(track.get('track').get('name'))
+    for playlist in playlists.get("items"):
+        go = True
+        j = 0
+        currId = playlist.get('id')
+        while (go):
+            songList = Sp.playlist_items(currId, offset=j * 100).get("items")
+            for song in songList:
+                if (song.get('track')):
+                    print(song.get('track').get('name'))
+            print(f"------LENGTH: {len(songList)}")
+            if len(songList)<100:
+                go=False
+            else:
+                j+=1
+                #print(songList)
+                #for songObj in songList:
+                   # print(songObj.get('name'))
 
 
 if __name__ == "__main__":
