115f5421378660bbd7d353ef016168f701ac41e9
linhara <linus_haraldsson@hotmail.com>
2021-03-21 16:02:53 +0100

    fixed bug looking at only last 100 song of top 500

--- main.py
+++ main.py
@@ -5,21 +5,29 @@ import spotipy.util as util
 playlist_url ="https://open.spotify.com/playlist/2YRe7HRKNRvXdJBp9nXFza?code=AQAQZcBQj5p_5ry1PxdeO6oLjBzIkd67MVLfhQul_0HMliIW5k3sx9OEtGWAuswEa94EjUpgZdQEhTYQttclGfHZN6boYLI4zDwu4CERkVpfY0W1FopucXdkhKnV0oPD_-kuv4yKrHI9VkhTSH_XuN5NPiBD8roa9pp_Eclw1I1mE_td9OH_urKanteQtvI8P6d_ZSiueUnRgJ17oZsjqNlLAVHlFxhb1CGlv57fKg"
 
 userName = input("Please enter Username: ")
-OAuth = sp.oauth2.SpotifyPKCE(client_id="5d6ba60172a84bf2966d5a073b3c02d1",
+OAuth = sp.oauth2.SpotifyPKCE(client_id="de445576e2884c55be39229d878d7ca1",
                                                username= userName,
                                                redirect_uri="http://localhost:9000",
                                                scope="user-library-read")
 
 Sp = sp.Spotify(auth_manager=OAuth)
+tracksList = []
+#---------------------------------------------------
+# The playlist we compare popularity against is a little over 500 songs.
+# therefore we can loop 6 times to get 5*100 + one last iteration with
+# the rest of the songs.
+#---------------------------------------------------
 for i in range(6):
-    tracks = Sp.playlist_items(playlist_url, offset = 100 * i).get('items')
-totalPopularity = 0
+    tracksList.append(Sp.playlist_items(playlist_url, offset = 100 * i).get('items'))
 
+totalPopularity = 0
 sumOfSongs = 0
-for item in tracks:
-    if item.get('track'):
-        sumOfSongs +=1
-        totalPopularity += item.get("track").get("popularity")
+
+for tracks in tracksList:
+    for item in tracks:
+        if item.get('track'):
+            sumOfSongs +=1
+            totalPopularity += item.get("track").get("popularity")
 
 averagePopularity = totalPopularity/sumOfSongs
 print(f"The average popularity is: {averagePopularity/2}")
